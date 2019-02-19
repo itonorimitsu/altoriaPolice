28233f8eafc3506a8fc099112343b673fa53de7b
伊藤永光 <itounorimitsu@itounagamitsunomacbook-pro.local>
2019-02-19 17:26:26 +0900

    Initial Commit

---
+++ altoriaPolice_new/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  altoriaPolice_new
+//
+//  Created by 伊藤永光 on 2019/02/19.
+//  Copyright © 2019 Nito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
